de9cfd8cacca306d98b83aa20a8190b45dffedd7
a-havu <havuaino@gmail.com>
2024-11-28 09:52:01 +0200

    Delete main_ft_strrchr.c

--- main_ft_strrchr.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-    int c = 'c';
-    const char str[] = "Backwards searching";
-
-    printf("strrchr: %s\n", strrchr(str, c));
-	printf("ft_strrchr: %s\n", ft_strrchr(str, c));
-
-}

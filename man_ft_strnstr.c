f5064837739b7ebba063ce413217339f8975d685
a-havu <havuaino@gmail.com>
2024-11-28 09:52:22 +0200

    Delete man_ft_strnstr.c

--- man_ft_strnstr.c
+++
@@ -1,9 +0,0 @@
-int main()
-{
-	const char big[] = "laila";
-	const char little[] = "il";
-	size_t len = 9;
-
-	printf("strnstr: %s\n", strnstr(big, little, len));
-	printf("ft_strnstr: %s\n", ft_strnstr(big, little, len));
-}
